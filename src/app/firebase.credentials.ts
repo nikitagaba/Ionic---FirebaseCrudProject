856ec113640dd752dc2c553ea80849747c438804
GABA Nikita <ngaba@one.steria.dom>
2018-01-29 15:04:40 +0530

    Project Version 1.0.0

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+ apiKey: "YOUR-API-KEY", 
+ authDomain: "YOUR-PROJECT-AUTHDOMAIN",
+ databaseURL: "YOUR-DB-URL",
+ projectId: "YOUR-PROJECT-ID",
+ storageBucket: "",
+ messagingSenderId: "YOUR-SENDER-ID"
+};
